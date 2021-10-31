721ca173562c24d0aa75ad2e3f0f1845d987c061
Kazaght <92064007+Kazaght@users.noreply.github.com>
2021-10-30 21:32:49 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+let llamarfuncion: string = String (prompt("ingrese A, B C, o D"))
+  function tabladel(numerodetabla: number) {
+    for (let contador = 1; contador <= 9; contador++) {
+      const resultado = contador * numerodetabla;
+      console.log(resultado);
+    }
+  while (llamarfuncion === a ||
+    llamarfuncion === b ||
+    llamarfuncion === c ||
+    llamarfuncion === d )
+    tabladel(9) ; {
+  
+  tabladel(9);
+  console.log(tabladel);
+} else {
+  console.log("Ingrese a,b,c o d");
+}
